4f098705cb2f6cef3efc82c79193968ac673811b
ManosFps1pt <manos.dragasakis@gmail.com>
2025-03-26 18:20:56 +0200

    changes

--- with_acceleration.py
+++ with_acceleration.py
@@ -13,7 +13,7 @@ MAX_VELOCITY = 5
 rect_acceleration: v2 = v2(0, 0)
 rect_velocity: v2 = v2(0, 0)
 rect_position: v2 = v2(0, 0)
-BASE_FPS = 60
+BASE_FPS = 0
 
 run = True
 while run:
@@ -21,31 +21,36 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+    if abs(rect_velocity.x) < .001:
+        rect_velocity.x = 0
+    if abs(rect_velocity.y) < .001:
+        rect_velocity.y = 0
     rect_acceleration = v2(0, 0)
     keys: tuple[bool] = pygame.key.get_pressed()
     acceleration_length = ACCELERATION * loop_time
     if acceleration_length == 0:
         continue
     if keys[pygame.K_UP]:
-        rect_acceleration.y = -acceleration_length
+        rect_acceleration.y -= acceleration_length
     if keys[pygame.K_DOWN]:
-        rect_acceleration.y = acceleration_length
+        rect_acceleration.y += acceleration_length
     if keys[pygame.K_LEFT]:
-        rect_acceleration.x = -acceleration_length
+        rect_acceleration.x -= acceleration_length
     if keys[pygame.K_RIGHT]:
-       rect_acceleration.x = acceleration_length
+       rect_acceleration.x += acceleration_length
     if rect_acceleration != v2(0, 0):
         rect_acceleration.scale_to_length(acceleration_length)
     rect_acceleration -= rect_velocity * FRICTION
     rect_velocity += rect_acceleration
     if rect_velocity != v2(0, 0) and rect_velocity.length() > MAX_VELOCITY:
         rect_velocity.scale_to_length(MAX_VELOCITY)
-    print(rect_velocity)
+
+    pygame.display.set_caption(f"{clock.get_fps():.2f} fps \t velocity: {rect_velocity}")
     rect_position += rect_velocity + .5 * rect_acceleration
     if rect_position.x<0: rect_position.x=1
     if rect_position.y<0: rect_position.y=1
-    if rect_position.x>SCREEN_SIZE.x-41: rect_position.x=SCREEN_SIZE.x-41
-    if rect_position.y>SCREEN_SIZE.y-21: rect_position.y=SCREEN_SIZE.y-21
+    if rect_position.x>SCREEN_SIZE.x-rect.width+1: rect_position.x=SCREEN_SIZE.x-rect.width+1
+    if rect_position.y>SCREEN_SIZE.y-rect.height+1: rect_position.y=SCREEN_SIZE.y-rect.height+1
     SCREEN.fill("#000000")
     rect.topleft = rect_position
     pygame.draw.rect(SCREEN, "#FFFFFF", rect)
--- without_acceleration.py
+++ without_acceleration.py
@@ -19,17 +19,25 @@ while run:
         if event.type == pygame.QUIT:
             run = False
     keys: tuple[bool] = pygame.key.get_pressed()
-    velocity = v2(0, 0)
+    rect_velocity = v2(0, 0)
     if keys[pygame.K_UP]:
-        velocity.x = MAX_VELOCITY
+        rect_velocity.y -= MAX_VELOCITY
     if keys[pygame.K_DOWN]:
-        velocity
+        rect_velocity.y += MAX_VELOCITY
+    if keys[pygame.K_LEFT]:
+        rect_velocity.x -= MAX_VELOCITY
+    if keys[pygame.K_RIGHT]:
+        rect_velocity.x += MAX_VELOCITY
     print(rect_velocity)
+    if rect_velocity != v2(0, 0) and rect_velocity.length() > MAX_VELOCITY:
+        rect_velocity.scale_to_length(MAX_VELOCITY)
     rect_position += rect_velocity
     if rect_position.x<0: rect_position.x=1
     if rect_position.y<0: rect_position.y=1
     if rect_position.x>SCREEN_SIZE.x-41: rect_position.x=SCREEN_SIZE.x-41
     if rect_position.y>SCREEN_SIZE.y-21: rect_position.y=SCREEN_SIZE.y-21
+    rect.topleft = rect_position
+    pygame.display.set_caption(f"{clock.get_fps():.2f} fps \t velocity: {rect_velocity}")
     SCREEN.fill("#000000")
     rect.topleft = rect_position
     pygame.draw.rect(SCREEN, "#FFFFFF", rect)
